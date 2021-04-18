ae00a6e630b0f65f4a1f1c9eb78dd835609b4cc9
Gohar Attiq <goharattiq@hotmail.com>
2021-04-19 04:56:40 +0500

    .env

--- src/redux/quote/thunk.js
+++ src/redux/quote/thunk.js
@@ -6,11 +6,13 @@ export const getQuote = key =>  dispatch => {
         mode: 'no-cors',
         headers:{
             'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
             'Content-Type':'application/json'
         }
     }
     axios
-    .get(`https://zenquotes.io/api/random/${key}`,config)
+    .get(`api/random/${key}`,config)
     .then(res =>{
         dispatch(setQuote(res.data[0]));
     })
